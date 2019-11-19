1a4321bd2946b9e7d5aa0b3b0413e4826d7af9f1
IN1DM3B_09 <IN1DM3B_09@H015112.NET>
2019-11-19 14:21:37 +0100

    Pequeños avances. IconoApp y poco mas

--- app/src/main/java/com/example/taskmanager/LoginActivity.java
+++ app/src/main/java/com/example/taskmanager/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUser;
+    private EditText etPassword;
+
+    SharedPreferences preferencias;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUser = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPwd);
+        preferencias = getSharedPreferences("usuario", Context.MODE_PRIVATE);
+        editor = preferencias.edit();
+        editor.putString("user","jon");
+        editor.putString("password", "1234");
+    }
+
+    public void login(View view){
+
+    }
+
+    public void registrar(View view){
+
     }
 }
